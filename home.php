bbc74b599fe9471c6f384235a55a28e5254774e8
taku795 <79009331+taku795@users.noreply.github.com>
2021-06-27 07:42:24 +0900

    home.phpの更新
    ファイルを作成したことによるaタグのパス変更のため更新します。

--- home.php
+++ home.php
@@ -1,4 +1,8 @@
 <?php require "header.php"; ?>
+
+<h1>趣味旅行</h1>
+<a href="menyu_page.php"><h2>メニュー</h2></a>
+<h2>掲示板</h2>
 <?php $pdo=new PDO('mysql:host=localhost;dbname=board;charset=utf8',
 'staff','password');
 foreach($sql=$pdo->query('select * from thread') as $row ) {
@@ -6,5 +10,6 @@ foreach($sql=$pdo->query('select * from thread') as $row ) {
     echo '<br>';
 }
 ?>
+<h2>記事</h2>
 
 <?php require "footer.php"; ?>
