dff1d44c6d433b21fd7d2a62d750f7433ee74713
taku795 <takufamilyd4@gmail.com>
2021-07-19 18:37:08 +0900

    テスト

--- login/login.php
+++ login/login.php
@@ -1,6 +1,11 @@
 <?php 
-$pdo=new PDO('mysql:host=localhost;dbname=board;charset=utf8',
-'staff','password');
+
+try{
+  $pdo=new PDO('mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_57d4f20f139d026;charset=utf8',
+'b0e1b2175788a4','46b12765');
+}catch(PDOException $e){
+  print('DB接続エラー:'.$e->getMessage());
+}
 session_start();
 
 // Googleログイン時
