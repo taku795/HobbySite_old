6753935beb1e55e245e7ad3ccd7c3b8f66e5d7b3
taku795 <takufamilyd4@gmail.com>
2021-09-06 18:13:38 +0900

    デザイン、投稿完了画面修正

--- post/post_end_page.php
+++ post/post_end_page.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="jp">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../css/post_end.css">
     <title>投稿完了画面</title>
 </head>
 <body>
-    <h3>投稿完了です</h3>
+    <p>投稿完了です</p>
     <a class="home_link" href="../home.php">ホーム画面へ</a>
 </body>
 </html>
