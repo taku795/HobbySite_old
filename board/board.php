4fb7967a05a83034e2afd484f5cfcdb9a0ba9949
taku795 <takufamilyd4@gmail.com>
2021-09-10 07:58:52 +0900

    掲示板画面修正、不要ファイル消去

--- board/board.php
+++ board/board.php
@@ -15,7 +15,7 @@ session_start();
 <!-- 入力フォーム -->
 <form class="postform" action="#" method='post' class="postform">
 表示名：<input type='text' name="name" placeholder="<?php echo $_SESSION['user_name']; ?>">
-内容：<input type="text" name='message' placeholder="テキストを入力" size="40">
+登校内容：<input type="text" name='message' placeholder="テキストを入力" size="40">
 <input type='hidden' value=<?php echo $_GET['thread_id']; ?> name='thread_id'>
 <input type='submit' value='投稿する'>
 </form>
