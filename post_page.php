4aba97e752425d7c694a118aa9affc2b4ec03b16
taku795 <takufamilyd4@gmail.com>
2021-09-18 05:53:16 +0900

    投稿画面にマップを追加

--- post_page.php
+++ post_page.php
@@ -21,21 +21,93 @@
         </div>
         <div class="content_form">
             <p>投稿内容</p>
+            <div class="mapform">
+                <button type="button" onclick="makeMap()">マップ</button>
+                マップを消去
+                <select id="mapnum">
+                </select>
+                <button type="button" onclick="deleteMap()">消去</button>
+            </div>
+            <div class="tag">
+                タグをつける：
+                <select name="tag">
+                <option value="">-</option>
+                <?php
+                //tag_masterからタグIDとnameを順番に
+                foreach($sql=$pdo->query('select * from tag_master') as $row ) {
+                    echo "<option value=$row[Tag_ID]>$row[Tag_Name]</option>";
+                }
+                ?>
+                </select>
+                <br>
+            </div>
             <textarea name="content" rows="10" placeholder="テキストを入力"></textarea>
-            タグをつける：
-            <select name="tag">
-            <option value="">-</option>
-            <?php
-            //tag_masterからタグIDとnameを順番に
-            foreach($sql=$pdo->query('select * from tag_master') as $row ) {
-                echo "<option value=$row[Tag_ID]>$row[Tag_Name]</option>";
-            }
-            ?>
-            </select>
-            <br>
+    
             <input type='submit' value='投稿する'>
-        </div>
-        
+        </div>     
     </form>
-</body>
+    
+    <script>
+        var num=1;
+        makeOption();
+        function makeOption() {
+            var select = document.getElementById('mapnum');  
+            //初期化
+            while( select.firstChild ){
+                select.removeChild(select.firstChild );
+            }
+            //numに応じてselectの子要素にoptionを追加
+            var n =0;
+            now_num=num-1;
+            for (n;n<=now_num;n++) {
+                var option = document.createElement('option');
+                if (n==0) {
+                option.value = "";
+                option.textContent = "-";
+                } else {
+                option.value = n;
+                option.textContent = n;
+                }
+                select.appendChild(option);
+            }     
+        }
+            
+        function deleteMap() {
+            var text=document.getElementById('textarea').value;
+            var mapnum = document.getElementById('mapnum').value;
+            var patern = new RegExp("\\\\\\\\\\\\マップ"+mapnum+":(.+)\\\\\\\\\\\\ ",'g');
+
+            if (text.match(patern)) {
+            text=text.replace(patern,"");
+            document.getElementById('textarea').value=text;
+            if (mapnum==num) {
+                num--;
+            } else {        
+                var buf;
+                for (buf=mapnum;buf<=num;buf++) {
+                var patern2 = new RegExp("\\\\\\\\\\\\マップ"+buf);
+                var buf_buf=buf-1;
+                var after = "\\\\\\マップ" +buf_buf;
+                text=text.replace(patern2,after);
+                document.getElementById('textarea').value=text;
+                }
+                num--;
+            }
+            }
+            makeOption();
+        }
+
+        function makeMap() {
+            var area = document.getElementById('textarea');
+            var text = '\\\\\\\マップ'+ num +':場所を入力\\\\\\\ '+ '\n';
+            //カーソルの位置を基準に前後を分割して、その間に文字列を挿入
+            area.value = area.value.substr(0, area.selectionStart)
+                + text
+                + area.value.substr(area.selectionStart);
+            num++;
+            makeOption();
+        }
+    </script>
+
+  </body>
 </html>
