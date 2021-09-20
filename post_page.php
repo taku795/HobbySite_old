9df92a0caf64ced2feaeaa60325abe3823fb95d9
taku795 <takufamilyd4@gmail.com>
2021-09-21 06:09:11 +0900

    編集機能修正、マップ機能修正

--- post_page.php
+++ post_page.php
@@ -17,7 +17,7 @@
     <h2>趣味に関することを自由に書いてみよう</h2>
     <form class="post_form" action='post/post.php' method='post'>
         <div class="title_form">
-        <p>タイトル</p><input type='text' name='title' id='title' placeholder="タイトルを入力">
+            <p>タイトル</p><input type='text' name='title' id='title' placeholder="タイトルを入力">
         </div>
         <div class="content_form">
             <p>投稿内容</p>
@@ -58,37 +58,49 @@
     <script>
         //編集処理
         <?php
-        $sql=$pdo->prepare("SELECT * FROM content WHERE id=?");
-        $sql -> execute([$edit_content_id]);
-        $result = $sql->fetchAll(PDO::FETCH_BOTH);
-        $title=$result[0]['Title'];
-        $content=$result[0]['Content'];
-        //コンテンツIDからついているタグを抽出
-        $sql=$pdo->prepare("SELECT * FROM tag_to_content WHERE Content_ID=?");
-        $sql -> execute([$edit_content_id]);
-        $result = $sql->fetchAll(PDO::FETCH_BOTH);
-        $tag=$result[0]['Tag_ID'];
-        ?>
-        var content = "<?php echo $content; ?>";
-        var title = "<?php echo $title; ?>";
-        var tag = <?php
-        if(isset($tag)) {
-            echo $tag;
-        } else {
-            echo 101;
-        } ?>;
-        if (typeof content != 'undefined') {
-            document.getElementById('textarea').value = content;
-            document.getElementById('title').value = title;
-            if (tag!=101) {
-                document.getElementById('tag').options[tag].selected = true;
+        if (isset($edit_content_id)) {
+            $sql=$pdo->prepare("SELECT * FROM content WHERE id=?");
+            $sql -> execute([$edit_content_id]);
+            $result = $sql->fetchAll(PDO::FETCH_BOTH);
+            $title=$result[0]['Title'];
+            $content=$result[0]['Content'];
+            //コンテンツIDからついているタグを抽出
+            $sql=$pdo->prepare("SELECT * FROM tag_to_content WHERE Content_ID=?");
+            $sql -> execute([$edit_content_id]);
+            $result = $sql->fetchAll(PDO::FETCH_BOTH);
+            $tag=$result[0]['Tag_ID'];
+
+            $patern = '/＼{3}マップ([1-9]):([ぁ-んァ-ヶー一-龠]+)＼{3}/u';
+            if (preg_match($patern,$content)) {
+                $map_num=preg_match_all($patern,$content);
+            }
+            $content = json_encode($content);
+            echo"
+            var content = $content;
+            var title = '$title';
+            ";
+            if ($tag!=null) {
+                echo "var content_tag = $tag;";
             }
         }
-    </script>
-    <script>
-        //マップ処理
-        //テキストエリアの中身を一度見てマップがないか確認
+        ?>
+        
         var num=1;
+        
+        if (typeof content != "undefined") {
+            //マップの数を数える
+            num = <?php echo $map_num+1; ?>;
+
+            //記事内容をセット
+            document.getElementById('textarea').value = content;
+            document.getElementById('title').value = title;
+            if (typeof content_tag != "undefined") {
+                console.log("tag's value is", content_tag);
+                document.getElementById('tag').options[content_tag].selected = true;
+            }                        
+        }
+   
+        //マップ処理        
         makeOption();
         function makeOption() {
             var select = document.getElementById('mapnum');  
@@ -115,7 +127,7 @@
         function deleteMap() {
             var text=document.getElementById('textarea').value;
             var mapnum = document.getElementById('mapnum').value;
-            var patern = new RegExp("\\\\\\\\\\\\マップ"+mapnum+":(.+)\\\\\\\\\\\\ ",'g');
+            var patern = new RegExp("＼＼＼マップ"+mapnum+":(.+)＼＼＼",'g');
 
             if (text.match(patern)) {
             text=text.replace(patern,"");
@@ -125,9 +137,9 @@
             } else {        
                 var buf;
                 for (buf=mapnum;buf<=num;buf++) {
-                var patern2 = new RegExp("\\\\\\\\\\\\マップ"+buf);
+                var patern2 = new RegExp("＼＼＼マップ"+buf);
                 var buf_buf=buf-1;
-                var after = "\\\\\\マップ" +buf_buf;
+                var after = "＼＼＼マップ" +buf_buf;
                 text=text.replace(patern2,after);
                 document.getElementById('textarea').value=text;
                 }
@@ -139,7 +151,7 @@
 
         function makeMap() {
             var area = document.getElementById('textarea');
-            var text = '\\\\\\\マップ'+ num +':場所を入力\\\\\\\ '+ '\n';
+            var text = '＼＼＼マップ'+ num +':場所を入力＼＼＼';
             //カーソルの位置を基準に前後を分割して、その間に文字列を挿入
             area.value = area.value.substr(0, area.selectionStart)
                 + text
